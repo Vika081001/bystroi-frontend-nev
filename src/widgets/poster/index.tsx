c44542ffb02d963dfb76ed931331d63ca3420d6b
Trolozor <mplololowka@mail.ru>
2025-12-12 16:49:05 +0300

    add_banners

--- src/widgets/poster/index.tsx
+++ src/widgets/poster/index.tsx
@@ -6,14 +6,14 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/shared/ui/kit/carousel";
+import Image from "next/image";
 
 const Poster = () => {
   const images = [
-    "https://avatars.mds.yandex.net/i?id=43f5a7e3ffdb81cbd567b3188c2d0b8f43fb4f5e-4901993-images-thumbs&n=13",
-    "https://avatars.mds.yandex.net/i?id=a1879461c68095cebf3496b1120dc785bc41d013-17843417-images-thumbs&n=13",
-    "https://avatars.mds.yandex.net/i?id=6b83bb5ccbcc8b18ce90a7ab5f29a68e6a877227-5296267-images-thumbs&n=13",
-    "https://avatars.mds.yandex.net/i?id=6a6e76f7822831cb3cd76efd35640cfde03ac07f-4236774-images-thumbs&n=13",
-    "https://avatars.mds.yandex.net/i?id=c9486682d5c1062d8a49d4bd160442f6a7b98c1f-12262266-images-thumbs&n=13"
+    "https://downloader.disk.yandex.ru/preview/e8ac1c0c9c12787776e1ef7ed557a288b28d552a6d9d2bfee7280c1deba4316d/693c54d7/Y2zqYNYYulNbsMSj0mOHdMQ3SnoOMwcXiZMUOSjbGMr_B-i3QKQ96aWVbnXBq71glklhK5K2pvUvaV_89eTG5g%3D%3D?uid=0&filename=Цветы%201%20%28без%20кнопки%29.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=0&tknv=v3&size=2048x2048",
+    "https://2.downloader.disk.yandex.ru/preview/d0d5c2341bff85839cac081d64fe383373776bd6b96f8200c72454f850e1cb3c/inf/ZP6QDlKe7FuBdd3K7ALL74vASdK58TSazBPqx3aWvRIXqzoymK6QwxGC-Njcn1VUCTXOL-x269-iQMrqdNLFdA%3D%3D?uid=807831434&filename=Цветы%202%20%28без%20кнопки%29.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=807831434&tknv=v3&size=1920x1080",
+    "https://2.downloader.disk.yandex.ru/preview/f92fe061be6b6454b87fcc4f7e8ca3eb249ff5378e2996b91cbd1a69a9d02962/inf/ILcQFLTv-fqSQAnYv587ui21r6FnRJmW00GZ_VacITfPFCCvDKBFKsGQNh1Z6DhnuuHcuoTWgl2sNFbRb6njtg%3D%3D?uid=807831434&filename=Еда%20%28без%20кнопки%29.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=807831434&tknv=v3&size=1920x1080",
+    "https://3.downloader.disk.yandex.ru/preview/a7f8da33006d242828d939600a9e762fbc5b70a26bf455ddd1bde7d4a96d204e/inf/tlJCXTUhoJVkfVp-0IjlA5e1YtepN6WUWXt3sNPu5i4ObIUKvyfreiSkV-1H4A8Y_WkMxFy3tgOY9ziyt_75og%3D%3D?uid=807831434&filename=Дом%20%28без%20кнопки%29.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=807831434&tknv=v3&size=1920x1080",
   ];
   
   return (
@@ -23,10 +23,12 @@ const Poster = () => {
           <CarouselContent>
             {images.map((image, index) => (
               <CarouselItem key={index} className="h-56 w-full">
-                <img
+                <Image
                   src={image}
                   className="w-full h-full object-fill"
                   alt={`Poster ${index + 1}`}
+                  width={1600}
+                  height={600}
                 />
               </CarouselItem>
             ))}
